37d8daeeabd9d8b4f0117fe899dffea17068809f
LakshaySindhu <137389809+LakshaySindhu@users.noreply.github.com>
2024-06-01 09:16:54 +0000

    Conversions and Operations

---
+++ 01_Basics/03_operations.js
@@ -0,0 +1,70 @@
+// ********************** Operations ***************************
+
+// let value = 3;
+// console.log("6" + value); // 63
+// console.log(value + "6"); // 36
+// console.log(value + 6); // 9
+// console.log(("3") + ("6")); // 36
+
+// let negValue = -(value);
+// console.log(negValue);
+
+// let str1 = "Lakshay";
+// let str2 = " Sindhu";
+
+// let str = str1 + str2;
+// console.log(str);
+
+// console.log(str1+3); // Lakshay3
+// console.log(str1*3); // NaN
+// console.log(str1-3); // NaN
+// console.log(str1%3); // NaN
+// console.log(str1/3); // NaN
+
+
+// let str3 = "a";
+// console.log(str3+null);
+// console.log(str3+undefined);
+
+// let str4 = "12";
+// console.log(str4+3); // 123
+// console.log(str4*2); // 24
+// console.log(str4-2); // 10
+// console.log(str4%5); // 2
+// console.log(str4/2); // 6
+// console.log(str4+null); //12null
+// console.log(str4+undefined); //12undefined
+
+// let numValue = 12;
+// console.log(numValue+3); // 15
+// console.log(numValue+"3"); // 123
+// console.log(numValue*3); // 36
+// console.log(numValue*"3"); // 36
+// console.log(numValue/3); // 4
+// console.log(numValue%3); // 0
+// console.log(numValue**2); // 144
+// console.log(numValue+null); // 12
+// console.log(numValue+undefined); // NaN
+
+// console.log(+true); // 1 -> Not a good practice
+// console.log(+""); // 0 -> Not a good practice
+
+let num1, num2, num3; 
+num1 = num2 = num3 ; // Not a good practice either 
+// Please write code that is more readable
+
+let gameCounter = 100;
+console.log(gameCounter); // 100
+gameCounter++; // 100
+console.log(gameCounter); // 101
+console.log(gameCounter++); // 101
+console.log(++gameCounter); // 103
+
+
+
+
+
+
+
+
+
