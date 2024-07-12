c0770f1e9215c723a741be1603d6d3591a916d30
LakshaySindhu <137389809+LakshaySindhu@users.noreply.github.com>
2024-07-12 06:32:56 +0000

    execution context in js

--- 03_Basics/execution_context.js
+++ 03_Basics/execution_context.js
@@ -13,3 +13,23 @@ function one(){
 }
 
 one();
+
+// Execution Context - Refers to the environement within which javascript code is executed 
+
+// 1. Global Execution Context
+// 2. Function Execution Context
+// 3. Eval Execution Context
+
+
+
+let num1 = 10;
+let num2 = 5;
+
+function addNum(val1,val2){
+    let total = val1+val2;
+    return total;
+}
+
+console.log(addNum(num1,num2));
+console.log(addNum(10,29));
+
