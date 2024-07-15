2722968bcc4d150dee070fac522c7f7cc2d2ec16
LakshaySindhu <137389809+LakshaySindhu@users.noreply.github.com>
2024-07-15 06:30:03 +0000

    conditionals in js

--- 04_Control_Flow/01_conditionals.js
+++ 04_Control_Flow/01_conditionals.js
@@ -1,3 +1,84 @@
-// if
+// if(true){
+//     // console.log("Lakshay");
+// }
+
+// // 2==2 returns true
+// if(2==2){
+//     // console.log("Ray");
+// }
+
+// // 2=="2" => returns true
+// if(2=="2"){
+//     // console.log("Sindhu");
+// }
+
+// // 2 === "2" => returns false 
+// // === strict equality, checks both values and reference or datatype
+// if(2 === "2"){
+//     // console.log("hey");
+// }
+
+// // Comparison operators 
+// // >,<,>=,<=,==,!=,===
+
+
+// const temp = 43;
+
+// if(temp<50){
+//     console.log("Temperature Less than 50");
+// }
+// else{
+//     console.log("Temperature greater than 50");
+// }
+
+
+// let score = 200;
+
+// if(score>100){
+//     const power = "fly";
+//     console.log(`User Power : ${power}`);
+// }
+
+// // console.log(`User Power : ${power}`); // Throws an error
+
+
+// let balance = 1000;
+
+// if(balance<500){
+//     console.log("Less than 500");
+// }
+// else if(balance<750){
+//     console.log("Less than 750 but greater than 500");
+
+// }
+// else if(balance<900){
+//     console.log("Less than 900 but greater than 750");
+// }
+// else{
+//     console.log("Greater than 900");
+// }
+
+const userLoggedIn = false;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+if(userLoggedIn || loggedInFromGoogle || loggedInFromEmail && debitCard){
+    console.log("Allowed to buy courses");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ 04_Control_Flow/02_switch.js
@@ -0,0 +1,87 @@
+let month = 1;
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("Apr");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("Aug");
+        break;
+    case 9:
+        console.log("Sept");
+        break;
+    case 10:
+        console.log("Oct");
+        break;
+    case 11:
+        console.log("Nov");
+        break;
+    case 12:
+        console.log("Dec");
+        break;
+    default:
+        console.log("Invalid Month");
+        break;
+}
+
+const strmonth = "march";
+
+switch (strmonth) {
+    case "jan":
+        console.log("1");
+        break;
+    case "feb":
+        console.log("2");
+        break;
+    case "march":
+        console.log("3");
+        break;
+    case "april":
+        console.log("4");
+        break;
+    case "may":
+        console.log("5");
+        break;
+    case "june":
+        console.log("6");
+        break;
+    case "july":
+        console.log("7");
+        break;
+    case "august":
+        console.log("8");
+        break;
+    case "september":
+        console.log("9");
+        break;
+    case "october":
+        console.log("10");
+        break;
+    case "november":
+        console.log("11");
+        break;
+    case "december":
+        console.log("12");
+        break;
+   
+    default:
+        break;
+}
