745c951349a8efe606b46ef9424aad5db68861ba
LakshaySindhu <137389809+LakshaySindhu@users.noreply.github.com>
2024-07-17 06:44:08 +0000

    Higher Order in js

---
+++ 05_Loops/04_higher_order.js
@@ -0,0 +1,44 @@
+const friends = ["Lakshay","Ray","Shubham","Yusuf","Ayushman","Yuvraj"];
+
+
+// forEach Higher Order function
+// friends.forEach(function(val) {
+//     console.log(val);
+// })
+
+// using arrow functions 
+
+friends.forEach((val)=> {
+    // console.log(val);
+})
+
+function printme(item){
+    console.log(item);
+}
+// function ka reference pass krna hai forEach me
+// friends.forEach(printme()); // Throws an error
+
+// friends.forEach(printme);
+
+friends.forEach( (item,index,arr) => {
+    // console.log(item,index,arr);
+})
+
+const Coding = [
+    {
+        language: "Javascript",
+        extension: "js"
+    },
+    {
+        language: "C++",
+        extension: "cpp"
+    },
+    {
+        language: "Python",
+        extension: "py"
+    },
+]
+
+Coding.forEach( (item) => {
+    console.log(`${item.language} : ${item.extension}`);
+})
