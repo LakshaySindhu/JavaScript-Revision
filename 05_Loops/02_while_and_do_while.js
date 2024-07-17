c5be48e3a9d4577be41b6645bff8bc79ac112778
LakshaySindhu <137389809+LakshaySindhu@users.noreply.github.com>
2024-07-17 05:19:50 +0000

    while and do while loops

---
+++ 05_Loops/02_while_and_do_while.js
@@ -0,0 +1,24 @@
+let index = 0;
+while(index <= 10){
+    // console.log(`Value of index is : ${index}`);
+    index = index + 2;
+}
+
+let myArr = ["Lakshay","Shubham","Yuvraj","Ayushman"];
+let i = 0;
+while(i<myArr.length){
+    // console.log(myArr[i]);
+    i++;
+}
+
+
+// do while
+
+let score = 11;
+
+do{
+    console.log(`Score is  : ${score}`);
+    score++;
+} while(score<=10);
+
+
