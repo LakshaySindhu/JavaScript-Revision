b3ae293376a1c2d4332235db89bf43564f21629e
LakshaySindhu <137389809+LakshaySindhu@users.noreply.github.com>
2024-07-19 19:58:42 +0000

    DOM Manipulationn in js

---
+++ 06_DOM/02_dom.js
@@ -0,0 +1,86 @@
+// document.getElementById('heading1'); // returns html element 
+
+// document.getElementById('heading1').id; // returns heading1
+
+// document.getElementById('heading1').class; // returns undefined
+
+// document.getElementById('heading1').className; // returns name of the class
+
+// document.getElementById('heading1').getAttribute('attribute name');
+
+// storing 
+// const title = document.getElementById('heading1');
+
+// manipulating style 
+// title.style.backgroundColor = "green";
+
+// title.style.padding = "15px";
+
+// title.style.borderRadius = "15px";
+
+// title.innerText; // returns text that is visible on web page 
+
+// title.textContent; // returns text that is not visible on the webpage also
+
+// title.innerHTML; // returns text with tags 
+// for eq : 'JavaScript Revisionn<span style="display: none;">Uploading Code onGitHub</span>'
+
+// document.getElementsByClassName('class name');
+
+// query selector using tag
+// document.querySelector('h2'); // returns the first h2 element
+
+// query selector 
+// document.querySelector('#heading1');
+
+// query selector
+// document.querySelector('.heading');
+
+// document.querySelector('input[type="passowrd"]');
+
+// nesting selection
+
+// const myul = document.querySelector('ul'); // returns the first ul of html document
+
+// myul.querySelector('li'); // returns the first listed item of unordered list
+
+// firstli.style.backgroundColor = "yellow";
+
+
+// NodeList
+// querySelectorAll() method return NodeList
+
+// const li = document.querySelectorAll('.list-item');
+
+// Iterating NodeList
+
+// li.forEach( (l) => {
+//     l.style.backgroundColor = "green";
+    
+// });
+
+// HTMLCollection
+// getElementByClassName method return HTMLCollection
+// For iterating the HTML Collection elements, first convert it to array
+// then use any of the methods for iterating array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
